76c8cf914f60815231a75041a43913c2320b41a7
Carlos jarero <pymeshub19@gmail.com>
2024-12-09 19:18:31 -0600

    Adding User Model

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class UserModel(AbstractUser):
+    email = models.EmailField(unique=True)
+    telephone = models.CharField(max_length=10, blank=True, null=True)
+    country = models.CharField(max_length=5, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
