3a3f2f4b123ea902002600c5c0151e1315cff0fc
Carlos jarero <pymeshub19@gmail.com>
2024-12-10 08:29:50 -0600

    Adding Swagger

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class UserModel(AbstractUser):
+class User(AbstractUser):
     email = models.EmailField(unique=True)
     telephone = models.CharField(max_length=10, blank=True, null=True)
     country = models.CharField(max_length=5, blank=True, null=True)
